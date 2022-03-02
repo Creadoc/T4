e442731da7e464ed16d4bfd42478e0c041f176f5
Austin Evans <70028812+aevans01@users.noreply.github.com>
2022-03-02 17:29:05 -0600

    merged branches

--- src/pages/ViewProfile.jsx
+++ src/pages/ViewProfile.jsx
@@ -8,8 +8,54 @@ import Profile from "../components/Profile";
 */
 function ViewProfile() {
   return (
-    <div className="profileContainer">
-      <Profile />
+    <div>
+      <div className="profilePic">
+        <p>
+          <img className="profileImg" src={img} />
+        </p>
+        <button className=" editBtn uploadBtn" variant="primary" type="submit">
+          Upload
+        </button>
+        <button className="editBtn removeBtn" variant="primary" type="submit">
+          Remove
+        </button>
+      </div>
+      <Form>
+        <Form.Group className="mb-3 fname" controlId="formBasicFname">
+          <Form.Label>First Name</Form.Label>
+          <Form.Control type="firstName" placeholder="Enter First Name" />
+        </Form.Group>
+
+        <Form.Group className="mb-3 lname" controlId="formBasicLname">
+          <Form.Label>Last Name</Form.Label>
+          <Form.Control type="lastName" placeholder="Enter Last Name" />
+        </Form.Group>
+
+        <Form.Group className="mb-3 email" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3 phone" controlId="formBasicPhone">
+          <Form.Label>Phone Number</Form.Label>
+          <Form.Control type="email" placeholder="Enter Phone Number" />
+          <Form.Text className="text-muted">
+            We'll never share your phone number with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3 pass" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox"></Form.Group>
+        <button className="updateBtn" variant="primary" type="submit">
+          Save Changes
+        </button>
+      </Form>
     </div>
   );
 }
